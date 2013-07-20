77098a0a6de924574dfcd056466984bf2216a5d9
yakovla <ubuntu@ip-172-31-5-93.us-west-2.compute.internal>
2013-07-20 10:05:04 +0000

    web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ app.use(express.logger());
 app.get('/', function(request, response) {
     fs = require('fs');
     var buf = fs.readFileSync('index.html');
-    response.send(buf.toString());
+    response.send('test');
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
