efd7097b99dac22f9ce19b864d2d79d12878c63c
yakovla <ubuntu@ip-172-31-5-93.us-west-2.compute.internal>
2013-07-20 10:24:20 +0000

    web.js

--- web.js
+++ web.js
@@ -6,8 +6,8 @@ app.get('/', function(request, response) {
     //var fs  = require('fs');
     //var buf = fs.readFileSync('index.html');
     response.send('test');
-    var port = process.env.PORT || 5000;
-    app.listen(port, function() {
+    });
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
 	console.log("Listening on " + port);
     });
-});
