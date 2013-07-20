3f30f48d36c4500d5a0c536bdce9a4500af1732f
yakovla <ubuntu@ip-172-31-5-93.us-west-2.compute.internal>
2013-07-20 10:02:51 +0000

    web.js

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
+    fs = require('fs');
     var buf = fs.readFileSync('index.html');
     response.send(buf.toString());
 var port = process.env.PORT || 5000;
